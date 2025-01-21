4a652524015a30217031b0ce543bedfce2689067
yuanqi2020 <hislota@outlook.com>
2025-01-22 00:04:46 +0800

    状态ok

--- check_db.py
+++
@@ -1,7 +0,0 @@
-from app import app, db, User
-
-with app.app_context():
-    # 获取User模型的所有列
-    columns = User.__table__.columns
-    for column in columns:
-        print(f"{column.name}: {column.type}") 
